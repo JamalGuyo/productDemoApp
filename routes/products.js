14d7e9f4ecaaa624d1976bd369a876aec5a4e6f3
Jamal Guyo <jamalguyo08@gmail.com>
2020-11-06 09:54:10 +0300

    create show route

--- routes/products.js
+++ routes/products.js
@@ -7,5 +7,11 @@ router.get('/', async(req, res)=>{
     const products = await Product.find({});
     res.render('products/index', {products})
 })
+// show route
+router.get('/:id', async(req, res) => {
+    const {id} = req.params;
+    const product = await Product.findById(id);
+    res.render('products/show', {product})
+})
 // 
 module.exports = router;
