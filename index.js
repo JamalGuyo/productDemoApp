8cab5659c3e7dbecab2701471e05974fbd1beb85
Jamal Guyo <jamalguyo08@gmail.com>
2020-11-06 08:35:56 +0300

    connect to products database

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ app = express(),
 path = require('path'),
 mongoose = require('mongoose')
 
+// mongoose connection
+mongoose.connect('mongodb://localhost:27017/productdb', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('database connection successful'))
+.catch((err) => console.log(err));
+
 //express configs
 app.set('view engine', "ejs");
 app.set('views', path.join(__dirname, 'views'))
